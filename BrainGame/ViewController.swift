9a6dd9a7b05fb32fe1487a22ca1e1df221194f53
Francis Tsang <frtsang40@gmail.com>
2022-02-18 15:00:28 -0800

    Initial Commit

---
+++ BrainGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BrainGame
+//
+//  Created by Francis Tsang on 2/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
